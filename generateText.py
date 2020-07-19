d3ae4fb014db16ab8c9a593ddd08b2321f12edb0
Ben Hawkyard <bhawkyard1@gmail.com>
2020-07-19 23:31:31 +0100

    nested functions

--- generateText.py
+++ generateText.py
@@ -6,48 +6,6 @@ import tensorflow as tf
 
 from . import common
 
-def generate_text(model, start_string):
-  # Evaluation step (generating text using the learned model)
-
-  # Number of characters to generate
-  num_generate = 1000
-
-  # Converting our start string to numbers (vectorizing)
-  input_eval = [char2idx[s] for s in start_string]
-  input_eval = tf.expand_dims(input_eval, 0)
-
-  # Empty string to store our results
-  text_generated = []
-
-  # Low temperatures results in more predictable text.
-  # Higher temperatures results in more surprising text.
-  # Experiment to find the best setting.
-  temperature = 0.2
-
-  # Here batch size == 1
-  model.reset_states()
-  for i in range(num_generate):
-      predictions = model(input_eval)
-      # remove the batch dimension
-      predictions = tf.squeeze(predictions, 0)
-
-      # using a categorical distribution to predict the character returned by the model
-      predictions = predictions / temperature
-      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
-
-      # We pass the predicted character as the next input to the model
-      # along with the previous hidden state
-      input_eval = tf.expand_dims([predicted_id], 0)
-
-      text_generated.append(idx2char[predicted_id])
-
-  return (start_string + "".join(text_generated))
-
-def get_start_string(text):
-  sentence = random.choice(text.split("."))
-  words = sentence.split(" ")
-  return " ".join(words[:min(len(words) - 1, 3)])
-
 def run():
   print(f"Using tensorflow {tf.__version__}")
 
@@ -69,6 +27,49 @@ def run():
   model = common.build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
   model.load_weights(tf.train.latest_checkpoint(common.CHECKPOINTS_DIR))
   model.build(tf.TensorShape([1, None]))
+
+  def generate_text(model, start_string):
+    # Evaluation step (generating text using the learned model)
+
+    # Number of characters to generate
+    num_generate = 1000
+
+    # Converting our start string to numbers (vectorizing)
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    # Empty string to store our results
+    text_generated = []
+
+    # Low temperatures results in more predictable text.
+    # Higher temperatures results in more surprising text.
+    # Experiment to find the best setting.
+    temperature = 0.2
+
+    # Here batch size == 1
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # using a categorical distribution to predict the character returned by the model
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+        # We pass the predicted character as the next input to the model
+        # along with the previous hidden state
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+
+    return (start_string + "".join(text_generated))
+
+  def get_start_string(text):
+    sentence = random.choice(text.split("."))
+    words = sentence.split(" ")
+    return " ".join(words[:min(len(words) - 1, 3)])
+
 
   start_string = get_start_string(text)
   print("START:",start_string,"\n")
